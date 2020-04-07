5ab5fe319c6ad67917c92ad2e06521817c57215b
Jasmine Lok <jasminellok@gmail.com>
2020-04-07 01:11:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = ["Milo", "Otis", "Garfield"]
   kitten.push(name)
   return kitten
 }
 
 function destructivelyPrependKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = ["Milo", "Otis", "Garfield"]
   kitten.unshift(name)
   return kitten
 }
 
 function destructivelyRemoveLastKitten() {
-  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = ["Milo", "Otis", "Garfield"]
   kitten.pop()
   return kitten
 }
 
 function destructivelyRemoveFirstKitten() {
-  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = ["Milo", "Otis", "Garfield"]
   kitten.shift()
   return kitten
 }
 
-var kittens = ["Milo", "Otis", "Garfield"]
-
 function appendKitten(name) {
+  kittens = ["Milo", "Otis", "Garfield"]
   new_kittens = kittens
   new_kittens.push(name)
   return new_kittens
 }
 
 function prependKitten(name) {
+  kittens = ["Milo", "Otis", "Garfield"]
   new_kittens = kittens
   new_kittens.unshift(name)
   return new_kittens
 }
 
 function removeLastKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
   new_kittens = kittens
   new_kittens.pop()
   return new_kittens
 }
 
 function removeFirstKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
   new_kittens = kittens
   new_kittens.shift()
   return new_kittens
