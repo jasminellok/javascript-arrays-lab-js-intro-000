94da999beae1d1d72b05cc368789c9b0047bf188
Jasmine Lok <jasminellok@gmail.com>
2020-04-07 01:24:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,24 +26,20 @@ function destructivelyRemoveFirstKitten() {
 
 function appendKitten(name) {
   kittens = ["Milo", "Otis", "Garfield"]
-  kittens.push(name)
-  return kittens
+  return [...kitten, name]
 }
 
 function prependKitten(name) {
   kittens = ["Milo", "Otis", "Garfield"]
-  kittens.unshift(name)
-  return kittens
+  return [name, ...kitten]
 }
 
 function removeLastKitten() {
   kittens = ["Milo", "Otis", "Garfield"]
-  kittens.pop()
-  return kittens
+  return kittens.slice(-1)
 }
 
 function removeFirstKitten() {
   kittens = ["Milo", "Otis", "Garfield"]
-  kittens.shift()
-  return kittens
+  return kittens.slice(1)
 }
